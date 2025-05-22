494254c6b803506f5003eb38a0c03387b18e0896
Steve Sewell <steve@builder.io>
2025-05-21 17:25:10 -0700

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { AppProvider, Frame } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import { Dashboard } from "./components/Dashboard/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <AppProvider i18n={{}}>
-      <Frame>
-        <Dashboard />
-      </Frame>
+      <BrowserRouter>
+        <Frame>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            {/* Add more routes here as needed */}
+          </Routes>
+        </Frame>
+      </BrowserRouter>
     </AppProvider>
   );
 }
