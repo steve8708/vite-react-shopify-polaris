59eb2f834ead1d72863ec14cd4b7791ab7772ae9
Steve Sewell <steve@builder.io>
2025-05-21 18:45:09 -0700

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { AppProvider, Frame } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import { Dashboard } from "./components/Dashboard/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
     <AppProvider i18n={{}}>
       <BrowserRouter>
         <Frame>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            {/* Add more routes here as needed */}
-          </Routes>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* Add more routes here as needed */}
+            </Routes>
+          </Layout>
         </Frame>
       </BrowserRouter>
     </AppProvider>
